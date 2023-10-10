792567f13bbe46a3634b4216f80f58f79665e92a
Kamil Szykula <kamilszykula@gmail.com>
2023-10-10 16:37:27 +0100

    added space to improve readability

--- C7/Exercise3/Exercise3.c
+++ C7/Exercise3/Exercise3.c
@@ -7,6 +7,6 @@ int main ()
     float SA;
     printf("Type the radius and height of cylinder seperated by a space\n");
     scanf("%d %d",&r, &h);
-    SA = (2*M_PI*(float)r*(float)r) + (2*M_PI*(float)r*(float)h);
+    SA = (2 * M_PI * (float)r * (float)r ) + (2 * M_PI * (float)r * (float)h);
     printf ("Surface Area = %.3f\n", SA); 
 }
